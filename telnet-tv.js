11b2dca0222519e0be75f70db8a5ea36d380a334
Richard Klafter <richard@zinc.com>
2017-05-06 18:59:07 -0700

    addded telnet-tv.js

---
+++ telnet-tv.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+var program = require('commander');
+var telnet = require('wez-telnet');
+
+program
+  .version('0.0.1')
+  .option('--port <n>', 'port to run server on (default 8080)', parseInt)
+  .parse(process.argv);
+
+var clients = new Set();
+function writeToClients(chunk){
+    Array.from(clients.values()).forEach((client)=>{
+        client.write(frame);
+    })
+}
+
+process.stdin.on('data', function(chunk) {
+    writeToClients(chunk);
+});
+
+var s = new telnet.Server(function(client) {
+    // I am the connection callback
+    clients.add(client);
+    console.log("connected term=%s %dx%d", client.term, client.windowSize[0], client.windowSize[1]);
+
+    client.on('data', function (buf) {
+      console.log("Connection data:", buf.toString('ascii'));
+    });
+    client.on('resize', function (width, height) {
+      console.log("Connection resized to %dx%d", width, height);
+    });
+    //client.on('interrupt', function(){});
+    client.on('close', function () {
+      clients.delete(client);
+      console.log("Connection Closed");
+    });
+ });
+ s.listen(program.port||8080);
